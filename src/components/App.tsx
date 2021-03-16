71ef6ee4f2c3232fbf8086fd393bb4f6ee896277
Tony Khaov <tony.khaov@gmail.com>
2021-03-16 17:52:46 +0100

    start css colors project

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function App() {
+  return (
+    <div className="border border-black">
+      <h1 className="text-2xl text-center uppercase">css colors</h1>
+    </div>
+  )
+}
+
+export default App
